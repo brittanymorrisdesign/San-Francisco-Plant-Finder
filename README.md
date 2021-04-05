135825b0fcce69cbeeec2c9e639c9a1b09071cfc
Brittany Morris <brittanymorrisdesign@gmail.com>
2021-04-04 20:47:12 -0400

    updated with eslint

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 # :herb: San Francisco Plant Finder
 The San Francisco Plant Finder is a resource for gardeners, designers, ecologists and anyone who is interested in greening neighborhoods, enhancing our urban ecology and surviving the drought. The Plant Finder recommends appropriate habitat-building plants for sidewalks, gardens and roofs that are adapted to San Francisco's unique environment and climate.
 
-# Data Source
-https://data.sfgov.org/Energy-and-Environment/San-Francisco-Plant-Finder-Data/vmnk-skih
-
 Github Page: https://github.com/brittanymorrisdesign/San-Francisco-Plant-Finder </br>
 Live Demo: </br>
+
+# Data Source
+https://data.sfgov.org/Energy-and-Environment/San-Francisco-Plant-Finder-Data/vmnk-skih
 
 # Objective
 A React-based Plant Finder Search app with React components, helper/util functions, an lifecycle methods to query and display plants based on user searches. The plants in the database include California natives and Mediterranean climate exotics. A large subset of the California natives are actually local San Francisco natives. We strongly recommend local natives since they provide the best habitat for local pollinators and other wildlife with whom they have co-evolved. San Francisco natives are the most closely adapted to the climate and environment of the San Francisco peninsula of course, and so they are the best in terms of water and soil conservation, ecosystem health, and overall sustainability.
--- src/App.js
+++ src/App.js
@@ -1,12 +1,6 @@
 import React from 'react'
 import BackgroundImage from './assets/background-01.jpg'
-import {
-	AppBar,
-	Toolbar,
-	Grid,
-	Typography,
-	makeStyles,
-} from '@material-ui/core'
+import { AppBar, Toolbar, Typography, makeStyles } from '@material-ui/core'
 import { SearchBar, PlantData } from './components/'
 
 const useStyles = makeStyles(theme => ({
@@ -61,23 +55,19 @@ export default function App() {
 				</Toolbar>
 			</AppBar>
 			<div className={classes.root}>
-				<Grid container spacing={3}>
-					<Grid item xs={12}>
-						<Typography className={classes.paperTitle}>Plant Finder</Typography>
-						<Typography className={classes.paperSubTitle}>
-							A resource for gardeners, designers, ecologists <br /> and anyone
-							who is interested in greening neighborhoods, <br /> enhancing our
-							urban ecology and surviving the drought.
-						</Typography>
-						<img
-							src={BackgroundImage}
-							className={classes.superHeroImg}
-							alt='plantImage'
-						/>
-						<SearchBar />
-						<PlantData />
-					</Grid>
-				</Grid>
+				<Typography className={classes.paperTitle}>Plant Finder</Typography>
+				<Typography className={classes.paperSubTitle}>
+					A resource for gardeners, designers, ecologists <br /> and anyone who
+					is interested in greening neighborhoods, <br /> enhancing our urban
+					ecology and surviving the drought.
+				</Typography>
+				<img
+					src={BackgroundImage}
+					className={classes.superHeroImg}
+					alt='plantImage'
+				/>
+				<SearchBar />
+				<PlantData />
 			</div>
 		</div>
 	)
